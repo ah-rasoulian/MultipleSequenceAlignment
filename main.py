8ad226b3c3a04e7fae274e6d1e110dc2ca1f34ea
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2021-04-24 14:03:52 +0430

    getting inputs.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import numpy
+
+
 def main():
-    pass
+    n = int(input())  # getting counts of sequences
+    sequences = []
+    for i in range(n):
+        sequences.append(input().upper())  # getting sequences that we are going to align
 
 
 if __name__ == '__main__':
